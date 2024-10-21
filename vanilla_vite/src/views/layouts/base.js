5b09e3b80afd008b522106761702674670766681
Pep <jjimen2@xtec.cat>
2024-10-21 15:47:34 +0200

    Dynamic DOM example

--- vanilla_vite/src/views/layouts/base.js
+++ vanilla_vite/src/views/layouts/base.js
@@ -11,10 +11,44 @@ export default {
     document.querySelector("#app").innerHTML = renderString(layout, data)
   },
   refreshUser() {
-    logger.debug("Refresh layout...")
+    logger.debug("Refresh layout using Nunjucks...")
     const userDiv = document.querySelector('#user')
     // TODO Get session data
-    const data = {"username": "FAKE"}
+    const data = { "username": "FAKE" }
     userDiv.innerHTML = renderString(template, data)    
+  },
+  refreshUserUsingDOM() {
+    logger.debug("Refresh layout using DOM...")
+    const userDiv = document.querySelector('#user')
+    // TODO Get session data
+    const data = { "username": "FAKE" }  
+    // Reset
+    userDiv.innerHTML = '';
+
+    if (data.username) {
+      // Hello <span>username</span>
+      const helloText = document.createElement('span');
+      helloText.textContent = 'Hello';        
+      const usernameSpan = document.createElement('span');
+      usernameSpan.textContent = ` ${data.username}`;
+      usernameSpan.classList.add('text-decoration-underline', 'ms-2');
+      // Logout button
+      const logoutBtn = document.createElement('a');
+      logoutBtn.href = '/logout';
+      logoutBtn.classList.add('btn', 'btn-secondary', 'ms-4');
+      logoutBtn.textContent = 'Logout';
+      // Insert elements
+      userDiv.appendChild(helloText);
+      userDiv.appendChild(usernameSpan);
+      userDiv.appendChild(logoutBtn);
+    } else {
+      // Login button
+      const loginBtn = document.createElement('a');
+      loginBtn.href = '/login';
+      loginBtn.classList.add('btn', 'btn-primary');
+      loginBtn.textContent = 'Login';
+      // Insert elements
+      userDiv.appendChild(loginBtn);
+    }
   }
 }
